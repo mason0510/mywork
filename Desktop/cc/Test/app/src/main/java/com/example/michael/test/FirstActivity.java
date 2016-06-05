da2ed30c9d95ade09e7b7f57445c087b0a6594e6
zhangxiaocong <zhangxiaocong_2011@yeah.net>
2016-06-05 10:48:20 +0800

    更改了代码

---
+++ Desktop/cc/Test/app/src/main/java/com/example/michael/test/FirstActivity.java
@@ -0,0 +1,13 @@
+package com.example.michael.test;
+
+import android.app.Activity;
+
+/**
+ * Created by Michael on 2016/6/5.
+ */
+
+public class FirstActivity {
+    public FirstActivity(Activity activity) {
+    }
+
+}
